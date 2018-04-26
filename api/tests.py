426c1acf5828582df8948e0455f960cd72dcb73d
DarthHamza <hamzamakia@gmail.com>
2018-04-26 12:04:27 +0300

    preped test files

--- api/tests.py
+++ api/tests.py
@@ -3,7 +3,12 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from rest_framework.test import APIRequestFactory
 from restaurants.models import Restaurant
-from .views import RestaurantListView
+from .views import (
+    RestaurantListView,
+    RestaurantDetailView,
+    RestaurantUpdateView,
+    RestaurantDeleteView,
+)
 
 class RestaurantAPITest(TestCase):
     def setUp(self):
@@ -40,6 +45,14 @@ class RestaurantAPITest(TestCase):
             logo="http://icons.veryicon.com/png/Movie%20%26%20TV/Free%20Star%20Wars/Darth%20Vader.png"
             )
 
+        self.restaurant_data = {
+            "owner":self.user,
+            "name":"Restaurant 1", 
+            "description":"This is Restaurant 1 updated",
+            "opening_time":"00:01:00",
+            "closing_time":"23:59:00",
+        }
+
     def test_restaurant_list_view(self):
         list_url = reverse("api-list")
         request = self.factory.get(list_url)
@@ -60,3 +73,32 @@ class RestaurantAPITest(TestCase):
             response.data
         )
         self.assertEqual(response.status_code, 200)
+
+    def test_restaurant_detail_view(self):
+        detail_url = reverse("api-detail", kwargs = {"restaurant_id": self.restaurant_1.id})
+        request = self.factory.get(detail_url)
+        response = RestaurantDetailView.as_view()(request, restaurant_id=self.restaurant_1.id)
+        self.assertEqual(
+            {
+                'id':self.restaurant_1.id,
+                'owner':self.restaurant_1.owner,
+                'name':self.restaurant_1.name,
+                'description':self.restaurant_1.description,
+                'opening_time': self.restaurant_1.opening_time,
+                'closing_time': self.restaurant_1.closing_time,
+            },
+            response.data
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_restaurant_update_view(self):
+        update_url = reverse("api-update", kwargs = {"restaurant_id": self.restaurant_1.id})
+        request = self.factory.put(update_url, data=self.data)
+        response = RestaurantUpdateView.as_view()(request, restaurant_id=self.restaurant_1.id)
+        self.assertEqual(response.status_code, 200)
+
+    def test_restaurant_delete_view(self):
+        delete_url = reverse("api-delete", kwargs = {"restaurant_id": self.restaurant_1.id})
+        request = self.factory.delete(delete_url)
+        response1 = RestaurantDeleteView.as_view()(request, restaurant_id=self.restaurant_1.id)
+        self.assertEqual(response1.status_code, 204)
