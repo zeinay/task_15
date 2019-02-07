67c37b6b2216da742534bfef50b2186eb5a11f59
Nabil Fares <nabilfares@Nabils-MacBook-Pro.local>
2019-02-07 19:58:55 +0300

    aipi

--- api/views.py
+++ api/views.py
@@ -1,20 +1,32 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
 from restaurants.models import Restaurant
-from .serializers import RestaurantListSerializer
+from .serializers import RestaurantListSerializer,RestaurantDetailSerializer,RestaurantUpdateSerializer
 
 class RestaurantListView(ListAPIView):
 	queryset = Restaurant.objects.all()
 	serializer_class = RestaurantListSerializer
 
 # Complete me
-class RestaurantDetailView():
+class RestaurantDetailView(RetrieveAPIView):
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantDetailSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'restaurant_id'
 
 
 
 # Complete me
-class RestaurantUpdateView():
+class RestaurantUpdateView(UpdateAPIView):
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantUpdateSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'restaurant_id'
 
 
 
 # Complete me
-class RestaurantDeleteView():
+class RestaurantDeleteView(DestroyAPIView):
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantListSerializer
+	lookup_field = 'id'
+	lookup_url_kwarg = 'restaurant_id'
